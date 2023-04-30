9256038cdec13fdfc1641870ab9e9d6ac8787f57
Bipin Khanal <bipin.khanal1050@gmail.com>
2023-04-30 22:48:42 +0545

    trucker updated

--- components/profileComp/trucker.js
+++ components/profileComp/trucker.js
@@ -14,9 +14,7 @@ const TruckerPage = () => {
         },
       })
       .then((res) => {
-        if (!truckerProduct) {
-          setTruckerProduct(res.data.data);
-        }
+        setTruckerProduct(res.data.data);
         console.log(res);
       })
       .catch((err) => {
@@ -52,45 +50,70 @@ const TruckerPage = () => {
       {!truckerProduct ? (
         <div>Loading...</div>
       ) : (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 self-center justify-center p-10 items-center w-full">
-          {truckerProduct.map((product, idx) => {
-            return (
-              <div
-                key={idx}
-                className="bg-white flex flex-col rounded-xl p-10 justify-center  ">
-                <div className="flex flex-col gap-5 justify-start items-start w-11/12">
-                  <div className="flex flex-row  w-full">
-                    <span className="w-2/5">Name</span>
-                    <span className="w-3/5">{product.productname}</span>
-                  </div>
-                  <div className="flex flex-row w-full ">
-                    <span className="w-2/5">Quantity</span>
-                    <span className="w-3/5">{product.quantity}</span>
-                  </div>
-                  <div className="flex flex-row w-full ">
-                    <span className="w-2/5">Status</span>
-                    <span className="w-3/5">{product.status}</span>
-                    {product.status == "Trucker Assigned" && (
-                      <button
-                        onClick={() => {
-                          handleEditStatus(product._id);
-                        }}>
-                        <EditOutlinedIcon />
-                      </button>
-                    )}
-                    {product.status == "Product dispatched from Agrotech" && (
-                      <button
-                        onClick={() => {
-                          handleEditStatus2(product._id);
-                        }}>
-                        <EditOutlinedIcon />
-                      </button>
-                    )}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+        <div className="self-center justify-center p-10 items-center w-full">
+          <table className="w-full box-shadow divide-y divide-gray-200 border border-gray-200">
+            <thead>
+              <tr
+                className=" text-white"
+                style={{ backgroundColor: "#3E9B05" }}>
+                {/* <th className="p-2 text-left">Select</th> */}
+                <th className="p-2 text-left">S.N.</th>
+                <th className="p-2 text-left">Name</th>
+                <th className="p-2 text-left">Quantity</th>
+                <th className="p-2 text-left">Status</th>
+                <th className="p-2 text-left">Edit</th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200">
+              {truckerProduct.map((product, idx) => {
+                return (
+                  <tr key={idx}>
+                    {/* <td className="p-2">
+                      <input
+                        type={"checkbox"}
+                        // className="text-[#3E9B05]"
+                        style={{ accentColor: "#3E9B05" }}
+                      />
+                    </td> */}
+                    <td className="p-2">{idx + 1}</td>
+                    <td className="p-2">{product.productname}</td>
+                    <td className="p-2">{product.quantity}</td>
+                    <td className="p-2">{product.status}</td>
+                    <td>
+                      <div className="w-1/2 flex justify-around item-center">
+                        {product.status == "Trucker Assigned" && (
+                          <button
+                            onClick={() => {
+                              handleEditStatus(product._id);
+                            }}>
+                            <EditOutlinedIcon />
+                          </button>
+                        )}
+                        {product.status ==
+                          "Product dispatched from Agrotech" && (
+                          <button
+                            onClick={() => {
+                              handleEditStatus2(product._id);
+                            }}>
+                            <EditOutlinedIcon />
+                          </button>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+              {!truckerProduct.length && (
+                <tr>
+                  <td className="p-2" colSpan={7}>
+                    <p className="text-sm text-center text-gray-500">
+                      No records found
+                    </p>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
